88f0e1569823a54adcca06b7e2e755c4368e3fd6
Dams-d <damien.duro8@gmail.com>
2022-03-07 11:03:53 +0100

    WIP update

--- src/wwPlugin.js
+++ src/wwPlugin.js
@@ -175,7 +175,7 @@ export default {
     async login(email, password) {
         try {
             this.cognitoUser = new CognitoUser({ Username: email, Pool: this.cognitoUserPool });
-            const storage = new CookieStorage({ domain: window.location.hostname });
+            const storage = new CookieStorage({ domain: window.location.hostname, secure: false });
 
             const { accessToken } = await new Promise((resolve, reject) =>
                 this.cognitoUser.authenticateUser(
